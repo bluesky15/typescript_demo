637ff6ce6e9c77155d303637036d010dcf421d54
Lalit Behera <lalit143.er@gmail.com>
2018-08-12 20:57:33 +0530

    how to declare a class in typescript

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log("hello world!");
+// console.log("hello world!");
+
+class Message{
+     msg: string;
+    constructor(msg:string){
+    this.msg = msg;
+    }
+    greet(){
+        console.log(this.msg);
+    }
+}
+
+var message = new Message("Hello there");
+message.greet();
